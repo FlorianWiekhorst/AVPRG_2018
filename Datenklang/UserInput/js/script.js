df52fd0c95b191b7f2bb244076c44d3427d221a3
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2018-11-13 14:57:31 +0100

    color Picker test value

--- Datenklang/UserInput/js/script.js
+++ Datenklang/UserInput/js/script.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
 
+	//Color Picker Change Watcher
+	var colorWell;
+	var defaultColor = "#0000ff";
+
+	window.addEventListener("load", startup, false);
+	function startup() {
+		  colorWell = document.querySelector("#colorWell");
+		  colorWell.value = defaultColor;
+		  colorWell.addEventListener("change", updateAll, false);
+		  colorWell.select();
+	}
+	
+	}function updateAll(event) {
+		  var colorTitle = document.querySelector("colorTitle");
+		  document.querySelectorAll("colorTitle").forEach(function(p) {
+			h3.style.color = event.target.value;
+		  });
+	}
+
+
+
   //Mobile Footer visibility
+  /*
   var x = window.matchMedia("(max-width: 700px)");
   if (x.matches){
     $(window).on("scroll",function(){
@@ -22,5 +44,5 @@ $(document).ready(function() {
   };
   else{
     document.body.style.backgroundColor = "yellow";
-  }
+  }  */
 });
